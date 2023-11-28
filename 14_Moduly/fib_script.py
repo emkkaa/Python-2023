166b08a82acd754cfeb6c730742bfe365671f3ad
emkkaa <marta.b.kowalik@gmail.com>
2023-11-28 15:40:13 +0100

    zmiany

--- 14_Moduly/fib_script.py
+++ 14_Moduly/fib_script.py
@@ -4,4 +4,6 @@ import fib_module
 if __name__ == '__main__':
     n = int(sys.argv[1])
     print(f'Pierwsze {n} liczb Fibonacciego')
-    print(fib_module.fib(n))
+    print(fib_module.fib(n)) #z modulu fib module funkcja fib
+    print(fib_module.__name__)
+    print(sys.argv)
