2440f5d469e596ed558f9f5f2b2794fd03f1cff9
emkkaa <marta.b.kowalik@gmail.com>
2023-11-29 09:50:55 +0100

    zmiany

--- 12_Wyjatki_i_obsluga_bledow/wyjatek.py
+++ 12_Wyjatki_i_obsluga_bledow/wyjatek.py
@@ -9,3 +9,33 @@ else:
     print("Koniec")
 finally:
     print("Finally")
+
+
+# wczytaj przy użyciu input liczbę; wypisz sumę jej cyfr; jeśli podano błędne wejście, poproś jeszcze raz
+
+
+liczba = 0
+while True:
+    try: liczba = input("Podaj liczbę naturalną: ")
+    liczba = int(liczba)
+    except ValueError as e:
+    print (f"{liczba} nie jest liczbą!")
+    else:
+        break
+    suma = 0
+    i = liczba
+    while i > 0:
+        suma += i%10 i //= 10
+        print (f'Suma cyfr podanej liczby {liczba} wynosi {suma}')
+
+##################################
+try:
+    n = int(input("Podaj liczbę:"))
+    print(f'Podałeś liczbę {n}')
+
+suma = 0
+while n > 0:
+    suma += n % 10
+    n //= 10
+
+print(suma)
