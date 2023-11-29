29a17dd244b31270b55825af4eaf19a3fa98d473
emkkaa <marta.b.kowalik@gmail.com>
2023-11-29 11:38:48 +0100

    zmiany

--- 15_Pliki_tekstowe/csv_example.py
+++ 15_Pliki_tekstowe/csv_example.py
@@ -1,6 +1,27 @@
-import csv
+import csv #csv - modul do wczytania tych plikow
 
-with open('data\\foods.csv') as csvfile:
+with open('data\\foods.csv') as csvfile: #with czyli otwieram plik, z pakietu csv uzywam dictreadera ktory czyta pola z csv jako slownik (ciąg slowników)
     reader = csv.DictReader(csvfile)
     for row in reader:
         print(row)
+
+
+
+# Stwórz czytnik plików CSV bez użycia modułu CSV
+#
+# napisz skrypt który otworzy plik z danymi csv (nazwa podana z CLI - command line interface)
+# wypisze jego zawartość oddzielając pola tabulacją \t
+
+
+import csv
+import os
+import sys
+print(os.getcwd())
+tab = '\t'
+n = 0
+with open(sys.argv[1]) as csvfile:
+    for line in csvfile:
+        print(f'{n:03d} {tab.join(line.strip().split(","))}')
+        n +=1
+
+
--- 16_Dane_zdalne/json_example.py
+++ 16_Dane_zdalne/json_example.py
@@ -1,4 +1,4 @@
-import json
+import json #Javascript object notation pozwala zamienic arbitralna strukture danych nanapis i jak ktos to dostanie to jest w stanie odtworzyc te strukture danych
 
 data = {'year': [2010, 2011, 2012, 2011, 2012, 2010, 2011, 2012],
         'team': ['Bears', 'Bears', 'Bears', 'Packers', 'Packers', 'Lions',
@@ -6,12 +6,12 @@ data = {'year': [2010, 2011, 2012, 2011, 2012, 2010, 2011, 2012],
         'wins': [11, 8, 10, 15, 11, 6, 10, 4],
         'losses': [5, 8, 6, 1, 5, 10, 6, 12]}
 
-s = json.dumps(data)
+s = json.dumps(data) #bierzde dane i zamienia nna stringa dump string zmiana struktury danych pytonskich na napis bo latwiej przerzucic przez siec
 print(s)
 print(len(s))
 print(type(s))
 print()
-data = json.loads(s)
+data = json.loads(s) #przerzucan dane przez siec i odbieram je loads
 print(data)
 print(len(data))
 print(type(data))
