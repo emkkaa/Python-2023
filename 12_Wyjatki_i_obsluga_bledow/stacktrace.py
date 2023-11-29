775e38d4da2b6a4c22735b33a5b4ffe0f6f4a838
emkkaa <marta.b.kowalik@gmail.com>
2023-11-29 10:17:01 +0100

    zmiany

--- 12_Wyjatki_i_obsluga_bledow/stacktrace.py
+++ 12_Wyjatki_i_obsluga_bledow/stacktrace.py
@@ -1,9 +1,9 @@
 from my_package import functions
 import traceback
 
-if __name__ == '__main__':
+if __name__ == '__main__': #tutaj glowny kod programu
     try:
-        functions.ok_function()
+        functions.ok_function() #cala logika mieszka w ok function (pakiet poznajemy po init)
     except IndexError as e:
         print(e)
         print(traceback.format_exc())
