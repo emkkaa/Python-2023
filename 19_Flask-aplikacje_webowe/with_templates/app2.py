765cfb5af662092b85c0abe1013d7e9d1aa7b666
emkkaa <marta.b.kowalik@gmail.com>
2023-11-30 16:07:06 +0100

    zmiany

--- 19_Flask-aplikacje_webowe/with_templates/app2.py
+++ 19_Flask-aplikacje_webowe/with_templates/app2.py
@@ -9,3 +9,17 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return render_template('index_param.html', title="To jest tytul z parametru")
+
+
+
+#Zadanie
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+
+@app.route('/test')
+def hello():
+    return render_template('index_param.html', title="To jest tytul z parametru")
